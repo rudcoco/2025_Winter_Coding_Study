2597770ae57a38c6fca13d7371124c0851e68105
rudcoco <rudcoco09@naver.com>
2026-01-11 14:51:01 +0900

    Feat: Solve 2920

---
+++ rudcoco/2026_01/Week02/0104_2920.py
@@ -0,0 +1,13 @@
+def main():
+    music = list(map(int, input().split()))
+
+    if music == [1, 2, 3, 4, 5, 6, 7, 8]:
+        print('ascending')
+    elif music == [8, 7, 6, 5, 4, 3, 2, 1]:
+        print('descending')
+    else:
+        print('mixed')
+
+
+if __name__ == '__main__':
+    main()
